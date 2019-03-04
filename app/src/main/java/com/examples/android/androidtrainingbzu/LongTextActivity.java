0e2aba4b5b9ac7b3bc95721d07285a7d70f6a9d3
Mohammad Abu Hmead <mohammad.kaied@hotmail.com>
2019-03-04 09:04:36 +0200

    Initial commit

---
+++ app/src/main/java/com/examples/android/androidtrainingbzu/LongTextActivity.java
@@ -0,0 +1,13 @@
+package com.examples.android.androidtrainingbzu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LongTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_long_text);
+    }
+}
---
+++ app/src/main/java/com/examples/android/androidtrainingbzu/MainActivity.java
@@ -0,0 +1,23 @@
+package com.examples.android.androidtrainingbzu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startNewActivity(View view) {
+        switch (view.getId()){
+            case R.id.btn_startLongActivity:
+                startActivity(new Intent(this,LongTextActivity.class));
+                break;
+        }
+    }
+}

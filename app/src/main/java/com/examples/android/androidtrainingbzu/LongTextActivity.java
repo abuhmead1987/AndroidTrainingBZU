4b1072c1c8a804010d49905313b90f0f797a8665
Mohammad Abu Hmead <mohammad.kaied@hotmail.com>
2019-03-04 09:07:13 +0200

    Initial Commit

--- app/src/main/java/com/examples/android/androidtrainingbzu/LongTextActivity.java
+++ app/src/main/java/com/examples/android/androidtrainingbzu/LongTextActivity.java
@@ -9,5 +9,6 @@ public class LongTextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_long_text);
+
     }
 }
